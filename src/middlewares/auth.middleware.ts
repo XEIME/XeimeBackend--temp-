39f129cdd8b3497565e83db5990eea4e7a8ea1af
Marcos Higildo <marcosmuangula@gmail.com>
2026-02-20 18:19:14 +0200

    feat: add auth middleware and express type extensions

---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,38 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken"
+
+interface TokenPayload {
+    id: string,
+    role: string,
+}
+
+export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader){
+        return res.status(401).json({error: "Token não fornecido"});
+    }
+
+    const  parts = authHeader.split(" ");
+
+    if(parts.length !== 2){
+        return res.status(401).json({error: "Erro no formato do Token"});
+    }
+
+    const [, token] = parts
+
+    if(!token){
+        return res.status(401).json({error: "Token malformado"})
+    }
+
+    try{
+        const secret = process.env.JWT_SECRET as string;
+        const decoded = jwt.verify(token, secret) as unknown as TokenPayload
+
+        req.user = {id: decoded.id, role: decoded.role};
+
+        return next();
+    } catch (error) {
+        return res.status(401).json({error: "Token inválido ou expirado"})
+    }
+};
