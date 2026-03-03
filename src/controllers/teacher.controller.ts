c8c4a9a4e4bd674dd9da5ee54eb1a382d90d47fe
Marcos Higildo <marcosmuangula@gmail.com>
2026-03-03 20:08:15 +0200

    feat: allow school admins to fetch their school's teacher list

--- src/controllers/teacher.controller.ts
+++ src/controllers/teacher.controller.ts
@@ -3,50 +3,69 @@ import { prisma } from "../lib/prisma";
 import bcrypt from "bcryptjs";
 import { Role } from "../../generated/prisma/enums";
 
+
 export const createTeacher = async (req: Request, res: Response) => {
-    try {
-        const {
-            teacherName,
-            teacherEmail,
-            teacherPhone,
-            teacherPassword
-        } = req.body;
-        const schoolId = req.user.schoolId
-        const role = Role.TEACHER;
+  try {
+    const { teacherName, teacherEmail, teacherPhone, teacherPassword } =
+      req.body;
+    const schoolId = req.user.schoolId;
+    const role = Role.TEACHER;
 
-        const existingUser = await prisma.user.findFirst({
-            where: {
-                OR: [
-                    {email: teacherEmail},
-                    {phone: teacherPhone}
-                ]
-            }
-        })
+    const existingUser = await prisma.user.findFirst({
+      where: {
+        OR: [{ email: teacherEmail }, { phone: teacherPhone }],
+      },
+    });
 
-        if(existingUser){
-            return res.status(400).json({
-                message: "Este e-mail ou número de telefone já está em uso por outro utilizador."
-            });
-        }
-
-        const hashedPassword = await bcrypt.hash(teacherPassword, 10);
-
-        const teacher = await prisma.user.create({
-            data: {
-                name: teacherName,
-                email: teacherEmail,
-                phone: teacherPhone,
-                password: hashedPassword,
-                schoolId,
-                role,
-            },
-            select: { id: true, name: true, email: true, role: true }
-        });
-
-        return res.status(201).json({message: "o professor foi criado com sucesso.", teacher});
-
-    }catch(error){
-        console.error(error);
-        return res.status(500).json({error: "Erro ao tentar cadastrar o professor"})
+    if (existingUser) {
+      return res.status(400).json({
+        message:
+          "Este e-mail ou número de telefone já está em uso por outro utilizador.",
+      });
     }
-}
+
+    const hashedPassword = await bcrypt.hash(teacherPassword, 10);
+
+    const teacher = await prisma.user.create({
+      data: {
+        name: teacherName,
+        email: teacherEmail,
+        phone: teacherPhone,
+        password: hashedPassword,
+        schoolId,
+        role,
+      },
+      select: { id: true, name: true, email: true, role: true },
+    });
+
+    return res
+      .status(201)
+      .json({ message: "o professor foi criado com sucesso.", teacher });
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(500)
+      .json({ error: "Erro ao tentar cadastrar o professor" });
+  }
+};
+
+export const listTeachers = async (req: Request, res: Response) => {
+  try {
+    const role = Role.TEACHER;
+    const schoolId = req.user.schoolId
+
+    const teachers = await prisma.user.findMany({
+      where: {
+        role: role,
+        schoolId: schoolId
+      },
+    });
+
+    return res.json({ teachers });
+
+  } catch (error) {
+    return res.status(500).json({
+        error: "Erro ao tentar carregar a lista de Professores"
+    })
+  }
+};
--- src/routes/teacher.routes.ts
+++ src/routes/teacher.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import { createTeacher } from "../controllers/teacher.controller";
+import { listTeachers } from "../controllers/teacher.controller";
 import { authMiddleware } from "../middlewares/auth.middleware";
 import { checkRole } from "../middlewares/role.middleware";
 import { Role } from "../../generated/prisma/enums";
 
+
 const router = Router();
 
 router.post("/", authMiddleware, checkRole([Role.SCHOOL_ADMIN]), createTeacher);
+router.get("/", authMiddleware, checkRole([Role.SCHOOL_ADMIN]), listTeachers)
 
 export default router;
